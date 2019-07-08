e35f8ab4c20e2cc5c7de43e38d581182a87f6351
Jarrod Gorham <jarrod.gorham@gmail.com>
2019-07-08 09:29:02 -0400

    added loop to go through each city

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+from fake_useragent import UserAgent
 
+ua = UserAgent()
 
-url = 'https://www.craigslist.org/about/sites'
+header = {'User-Agent':str(ua.firefox)}
 
-result = requests.get(url)
+start_endpoint = 'https://www.craigslist.org/about/sites'
+
+result = requests.get(start_endpoint, headers=header)
 content = result.content
 
 soup = BeautifulSoup(content, features='html.parser')
@@ -12,9 +16,30 @@ divs = soup.find_all('div', {'class': 'colmask'})
 us_locations =divs[0]
 cities = us_locations.find_all('li')
 
-print cities[0]
-city_link = cities[0].find('a')
-print city_link['href']
 
-gigs_url = 'd/computer-gigs/search/cpg'
-print '{}{}'.format(city_link['href'], gigs_url)
+# print cities[10]
+# city_link = cities[10].find('a')
+# print city_link['href']
+
+
+gigs_param = 'd/computer-gigs/search/cpg'
+
+query = raw_input('Enter search term: ')
+search_query = '?query={}&is_paid=all'.format(query)
+
+
+for i in range(0,len(cities)):
+    city_link = cities[i].find('a')
+    gigs_url =  '{}{}'.format(city_link['href'], gigs_param)
+    complete_url = '{}{}'.format(gigs_url, search_query)
+
+    res = requests.get(complete_url, headers=header)
+    data = res.content
+    newsoup = BeautifulSoup(data, features='html.parser')
+
+    results = newsoup.find_all('a', {'class': 'result-title'})
+    result
+
+    if results:
+        for r in results:
+            print r.text, r.attrs['data-id']
