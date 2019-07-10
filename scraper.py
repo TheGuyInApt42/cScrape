76bc1e5a7090f2688d8e4d779d97150bfc286bed
Jarrod Gorham <jarrod.gorham@gmail.com>
2019-07-10 18:50:36 -0400

    cleaned up code some; added comments

--- scraper.py
+++ scraper.py
@@ -1,3 +1,9 @@
+'''
+This script allows searching of Craigslist computer gigs either manually searching a specific city 
+or automatically searching every city listed in the US.
+
+'''
+
 from bs4 import BeautifulSoup
 import requests
 from fake_useragent import UserAgent
@@ -6,98 +12,126 @@ import random
 import datetime
 from pymongo import MongoClient
 
-ua = UserAgent()
+ua = UserAgent() #instantiate user agent object for setting user agent strings
+
+# connect to database
 client = MongoClient()
 db = client.cScrape
 gigs = db.Gigs
 
 
-wait_time = random.randint(30, 60) 
-# print 'header is {} and initial wait time is {} secs'.format(ua.random, wait_time)
+wait_time = random.randint(30, 60) #set wait time for random time between 30 - 60 secs
 
-start_endpoint = 'https://www.craigslist.org/about/sites'
-gigs_param = 'd/computer-gigs/search/cpg'
+start_endpoint = 'https://www.craigslist.org/about/sites' # Craigslist locations url
+gigs_param = 'd/computer-gigs/search/cpg' # url parameter that gets appended for computer gigs 
 
 
 def process_url(url, tag, className):
+    ''' 
+    Processes url and returns a list of a specific tag with certain class, i.e list of all 'divs' with class name 'container'
+    Takes a url, HTML tag, class name of tag searching for
+    Returns list of bs4 elements that fit the desired criteria
+    '''
     header = {'User-Agent':str(ua.random)} # get random header string
-    result = requests.get(url, headers=header)
+    # print('Header for this request is {}'.format(header))
+    result = requests.get(url, headers=header) #load url with random header string
     content = result.content
 
     soup = BeautifulSoup(content, features='html.parser')
     return soup.find_all(tag, {'class': className})
     
 
-
 def get_cities(processed_data):
-    us_locations = processed_data[0]
+    '''
+    Gets all US cities links Returns list of line tags of all US cities
+    Takes in processed data which is a list produced from process_url function
+    Returns list of bs4 line elements
+    TODO: can maybe extend to other countries
+    '''
+    us_locations = processed_data[0] # gets first div in list which is the US
     return us_locations.find_all('li')
 
 
-def get_specific_city(cities_list, city='new york'):
+def get_city_index(cities_list, city='new york'):
+    '''
+    Gets index number of a specific city in the list of cities
+    Takes in a list containing the cities and a city to search for (Default is NYC)
+    Returns index number
+    '''
     for idx, elem in enumerate(cities_list):
         if city in elem.text:
             return idx
 
-def get_result_rows(info, city):
-    if info:
-        for r in info:
-            print('Wait time in second loop is {} seconds'.format(wait_time))
-            time.sleep(wait_time) #wait 30 - 60 secs in between
-            post_time = r.find('time').attrs['datetime']
-            title = r.find('a').text
-            post_id = r.find('a').attrs['data-id']
-            print(post_id, post_time, title, city)
+def get_result_rows(results, city, search_type='auto'):
+    '''
+    Checks if there are results and if so, then loops through each result while waiting a random time in between
+    Inserts row info into database if it is not already in db
+    Takes in list of rows, current city being processed and whether search type is auto or manual
+    '''
+    wait_time = 5 # wait 5 secs between each iteration (should be on same page so dont have to fire anymore new requests)
+    if results:
+        print('Found {} results'.format(len(results))) # print number of results
+        print('Current wait time between results is {} seconds'.format(wait_time))
+        for result in results:
+            time.sleep(wait_time) 
+            post_time = result.find('time').attrs['datetime']
+            title = result.find('a').text
+            post_id = result.find('a').attrs['data-id']
+            if search_type == 'manual':
+                print('The position is {} in {} was posted on {}'.format(title, city, post_time))
 
-            gig = {
-                "c_id": post_id,
-                "post_title": title,
-                "city": city,
-                "post_date": post_time,
-                "insert_date": datetime.datetime.utcnow()
-            }
-
-            old_gig = gigs.find_one({'c_id': post_id})
-            if old_gig:
-                print('Already seen.')
             else:
-                gig_id = gigs.insert_one(gig).inserted_id
+                #gig object to insert into database
+                gig = {
+                    "c_id": post_id,
+                    "post_title": title,
+                    "city": city,
+                    "post_date": post_time,
+                    "insert_date": datetime.datetime.utcnow()
+                }
+
+                old_gig = gigs.find_one({'c_id': post_id}) # check if gig is already in database
+                if old_gig:
+                    print('This gig is already in database')
+                else:
+                    gig_id = gigs.insert_one(gig).inserted_id # insert into database if not in it already
+                    print('Gig {} has been inserted into database'.format(gig_id))
             
-            
-        print('Returned {} results'.format(len(info)))
+        
     else:
-        print('No results.')
+        print('No results found.')
 
 #TODO: think about adding search params i.e search titles only
 def doSearch(cities_list, wait_time, search_query, search_type='auto'):
+    '''
+    Performs either targeted search or automatic search of all cities
+    Takes in list of cities, initial wait time, term to search for, and whether search is manual or auto
+    '''
     if search_type == 'manual':
         location = input('Enter city to check: ')
-        city_index = get_specific_city(cities_list, location)
+        city_index = get_city_index(cities_list, location)
         city_link = cities_list[city_index].find('a')
         current_city = cities_list[city_index].text
         print('Checking {}'.format(current_city)) # show what city is being checked
-        gigs_url =  '{}{}'.format(city_link['href'], gigs_param)
+        gigs_url =  '{}{}'.format(city_link['href'], gigs_param) # append computer gig parameter to city link
         complete_url = '{}{}'.format(gigs_url, search_query)
         info = process_url(complete_url, 'p', 'result-info')
-        get_result_rows(info, current_city)
+        get_result_rows(info, current_city,'manual')
     
     elif search_type == 'auto':
         for i in range(0,len(cities_list)):
-            print('Wait time in first loop for city {} is {} secs'.format(i, wait_time))
+            print('Current wait time for next result is {} secs'.format(wait_time))
+            print('Checking {}'.format(current_city))# show what city is being checked
             time.sleep(wait_time) #wait between 30 - 1 minutes before each
             
             city_link = cities_list[i].find('a')
             current_city = cities_list[i].text
-            print('Checking {}'.format(current_city))# show what city is being checked
             gigs_url =  '{}{}'.format(city_link['href'], gigs_param)
             complete_url = '{}{}'.format(gigs_url, search_query)
             info = process_url(complete_url, 'p', 'result-info')
             get_result_rows(info, current_city)
             wait_time = random.randint(30, 60) 
     
-
-
-
 
 ''' Start script '''
 data = process_url(start_endpoint, 'div', 'colmask')
